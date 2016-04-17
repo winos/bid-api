441034219e9803f1d23ac02fe55855de76c36911
Winos <dawinos@gmail.com>
2016-04-16 20:13:39 -0500

    format code

--- service/index.js
+++ service/index.js
@@ -8,103 +8,96 @@ const _ = require('underscore')
 module.exports = (io, isDev, Timer) => {
 
 	function finishAuction (error, auction) {
-		if (error) throw new Error('An error has ocurred')
-		AuctionDao.model
-		.findByIdAndUpdate(auction.id,
-			{'active': false, 'winner': {'user':'Dawin Ossa'}},
-			{safe: true, upsert: true, new : true},
-			(err, result) => {
-				if (err) throw new Error(err)
+	if (error) throw new Error('An error has ocurred')
+	AuctionDao.model.findByIdAndUpdate(auction.id,
+	{'active': false, 'winner': {'user':'Dawin Ossa'}},
+	{safe: true, upsert: true, new : true},
+	(err, result) => {
+		if (err) throw new Error(err)
 
-				result = result.toObject()
+		result = result.toObject()
+		result = _.omit(result, ['product', 'credits'])
 
-				result = _.omit(result,
-					['product', 'credits'])
+		var bid = _.last(result.bids)
 
-					var bid = _.last(result.bids)
+		var response = {
+			winner: {
+				user: result.winner
+			},
+			auction_id:result._id,
+			status: true
+		}
 
-					var response = {
-						winner: {
-							user: result.winner
-						},
-						auction_id:result._id,
-						status: true
-					}
+		io.emit('auction:winner', response)
 
-					io.emit('auction:winner', response)
+		if (isDev) console.log('finish auction', response)
+		})
+	}
 
-					if (isDev)
-					console.log('finish auction', response)
-				})
-			}
+	function changeTimeAuction (auction) {
+		io.emit('auction:changetime', auction)
+		if (isDev) console.log('auction:change Time', auction)
+	}
 
-			function changeTimeAuction (auction) {
-				io.emit('auction:changetime', auction)
-				if (isDev) console.log('auction:change Time', auction)
-			}
+	function bidAuction (data, fn) {
+		var newbid = {
+			idUser: data.user.id,
+			user: data.user.username
+		}
 
-			function bidAuction (data, fn) {
+		if (Timer.isActive) {
 
-				var newbid = {
-					idUser: data.user.id,
-					user: data.user.username
-				}
+			AuctionDao.model.findByIdAndUpdate(data.auction,
+				{ $push: {'bids':newbid}, $inc: {price: 50}},
+				{safe: true, upsert: true, new : true},
+				(err, result) => {
+					if (err) throw new Error('Error')
 
-				if (Timer.isActive) {
+					result = result.toObject()
 
-					AuctionDao.model
-					.findByIdAndUpdate(data.auction,
-						{ $push: {'bids':newbid}, $inc: {price: 50}},
-						{safe: true, upsert: true, new : true},
-						(err, result) => {
-							if (err) throw new Error('Error')
+					result = _.omit(result, ['product','active'])
 
-							result = result.toObject()
+						var bid = _.last(result.bids)
 
-							result = _.omit(result,
-								['product','active'])
-
-								var bid = _.last(result.bids)
-
-								var response = {
-									username: bid.user,
-									time: bid.time,
-									auction_id:result._id,
-									price: result.price,
-									time_init: result.time_rules.init
-								}
-
-								// reset timer...
-								Timer.reset(result._id, result.time_rules.init)
-
-								// register transaction initial
-								TransactionDao.save({
-									user: newbid.idUser,
-									quantity: result.credits_required,
-									type: "Spend",
-									sign: 50
-								}, function (transaction) {
-
-									if (transaction) {
-										fn({reset: true})
-										refresh(response)
-									}
-								})
-							})
+						var response = {
+							username: bid.user,
+							time: bid.time,
+							auction_id:result._id,
+							price: result.price,
+							time_init: result.time_rules.init
 						}
-					}
 
-					function refresh (data) {
-						io.emit('auction:refresh', data)
-					}
+						// reset timer...
+						Timer.reset(result._id, result.time_rules.init)
 
-					function registerWinner (data) {
-						io.emit('auction:winner', data)
-					}
+						// register transaction initial
+						TransactionDao.save({
+							user: newbid.idUser,
+							quantity: result.credits_required,
+							type: "Spend",
+							sign: 50
+						}, function (transaction) {
 
-					return {
-						bidAuction:bidAuction,
-						finishAuction: finishAuction,
-						changeTimeAuction: changeTimeAuction
-					}
+							if (transaction) {
+								fn({reset: true})
+								refresh(response)
+							}
+						})
+					})
 				}
+			}
+
+			function refresh (data) {
+				io.emit('auction:refresh', data)
+			}
+
+			function registerWinner (data) {
+				io.emit('auction:winner', data)
+			}
+
+			return {
+				bidAuction:bidAuction,
+				finishAuction: finishAuction,
+				changeTimeAuction: changeTimeAuction
+			}
+	}
